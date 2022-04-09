0d36db7a2a0ef2a9c845373d158e4eb7072d964f
minMin <goals1005@naver.com>
2022-04-09 23:56:43 +0900

    순수 서블릿 컨테이너 예외처리(스프링X)

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");//404가 발생하면 /error-page/400 URI 이동(컨트롤러 호출)
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500"); //RuntimeException 자식 예외까지 다 처리됨
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+
+
+    }
+}
---
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -0,0 +1,43 @@
+package hello.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 에러가 발생하는 페이지 만들기(테스트는 해봐야하니까)
+ */
+@Slf4j
+@Controller
+public class ServletExController {
+
+    /**
+     * 예외 발생 프름
+     * - WAS(여기까지 전파) < 필터 < 서블릿 < 인터셉터 < 컨트롤러(예외발생!)
+     */
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외 발생!");
+    }
+
+    /**
+     * sendError 흐름
+     * - WAS(sendError 호출 기록 확인) < 필터 < 서블릿 < 인터셉터 < 컨트롤러(response.sendError)
+     * WAS는 해당 예외를 처리하는 오류 페이지 정보를 확인한다.
+     * (WebServerCustomizer.java > new ErrorPage(RuntimeException.class, "error-page/404")
+     * - WAS '/error-page/500' 다시 요청 > 필터 > 서블릿 > 인터셉터 > 컨트롤러(/error-page/404) > View
+     * 서버 내부에서 다시 요청이 된다. (총 필터, 서블릿, 인터셉터, 컨트롤러가 2번씩 호출된다)
+     */
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류!"); //response에 상태 저장해서 보냄
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
+}
