06e0dcf1e72caf6d8cb4e6eb7224fc6f188f7e5f
minMin <goals1005@naver.com>
2022-04-12 14:09:28 +0900

    스프링 ExceptionHandler 사용 (ResponseStatusEx, DefaultHandlerEx)

---
+++ src/main/java/hello/exception/BadRequestException.java
@@ -0,0 +1,10 @@
+package hello.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")//400
+public class BadRequestException extends RuntimeException {
+
+
+}
